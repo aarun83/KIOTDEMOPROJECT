d39084772c9b06f80dacd4b4f9df1ffdcf2ee106
'Arunkumar' <aarun.83@gmail.com>
2025-02-05 15:57:31 +0530

    First Commit

---
+++ JavaBasicsJenkins/src/com/git/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.git;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+					System.out.println("Hello World");
+				// TODO Auto-generated method stub
+
+
+		}
+
+
+	}
